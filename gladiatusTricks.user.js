7799d091c5a47e9c039da364d1121da557e77d38
Aveneid <dj.exers@gmail.com>
2021-12-20 09:23:10 +0100

    Update gladiatusTricks.user.js

--- gladiatusTricks.user.js
+++ gladiatusTricks.user.js
@@ -16,17 +16,16 @@
 
 // ==/UserScript==
 
-(function() {
+var gladiatusTricks =  (function() {
 	'use strict';
 
-	var storage = window.localStorage;
 	var config ={
 		GT_removeBanner: false,
 		GT_getGold: false,
 		GT_set24H: false
 	};
 
-	var htmlMenu = "<div class='menuMain'><span class='menuOpen'>Gladiatus Tricks Menu</span><div id='menuDrop' class='menuBackground' style='display: none;'><ul><li>Remove banner <div class='menuButton menuButtonGreen' id='GT_removeBanner'>On<div><li> <li>Get gold form packages  <div class='menuButton menuButtonGreen' id='GT_gold' >On<div><li> <li>Set listing for 24H <div class='menuButton menuButtonGreen' id='GT_set24H'>On<div><li> </ul><span style='width: auto;' class='menuButton' id='GT_info'>Gladiatus Tricks by Aveneid</span></div></div>";
+	var htmlMenu = "<div class='menuMain'><span class='menuOpen'>Gladiatus Tricks Menu</span><div id='menuDrop' class='menuBackground' style='display: none;'><ul><li>Remove banner <div class='menuButton menuButtonGreen' id='GT_removeBanner'>On<div><li> <li>Get gold form packages  <div class='menuButton menuButtonGreen' id='GT_getGold' >On<div><li> <li>Set listing for 24H <div class='menuButton menuButtonGreen' id='GT_set24H'>On<div><li> </ul><span style='width: auto;' class='menuButton' id='GT_info'>Gladiatus Tricks by Aveneid</span></div></div>";
 
 	function createWindow(){
 		//create menu window
@@ -37,8 +36,8 @@
 		});
 
 
-		$("#GT_gold").click(function(){
-			setCfg("gold");
+		$("#GT_getGold").click(function(){
+			setCfg("getGold");
 		});
 		$("#GT_removeBanner").click(function(){
 			setCfg("removeBanner");
@@ -50,50 +49,57 @@
 
 
 	//helpers
+	localStorage.setItem("GT_gladiatusTricks",true);
 	function loadCfg(){
-		if(storage.getItem("GT_gladiatusTricks")){
+
+		if(castToBool(localStorage.getItem("GT_gladiatusTricks"))==true){
 			//load config from localStorage
-			Object.keys(storage).forEach(function(keys){if(keys.substring(0,3)=="GT_") config[keys]=storage[keys];})
-			Object.keys(config).forEach(function(keys){if(config[keys]==true){ $("div#"+keys).removeClass("menuButtonGreen");$("div#"+keys).addClass("menuButtonRed");$("div#GT_removeBanner").text("Off");}});
+			debugger;
+			Object.keys(localStorage).forEach(function(keys){ if(keys.substring(0,3)=="GT_") config[keys]=castToBool(localStorage[keys]); });
+			debugger;
+			Object.keys(config).forEach(function(keys){
+				console.log(keys +":"+ config[keys]);
+				if(castToBool(config[keys])==true){
+					$("div#"+keys).removeClass("menuButtonGreen");
+					$("div#"+keys).addClass("menuButtonRed");
+					$("div#"+keys).text("Off");
+				}});
 		}
+
 	}
 	function saveCfg(){
-		//save config to local storage
-		Object.keys(config).forEach(function(key){storage.setItem(key,config[key]);})
-		storage.setItem("GT_gladiatusTricks",true);
+		//save config to local localStorage
+		Object.keys(config).forEach(function(key){localStorage.setItem(key,config[key]);})
 	}
+	function castToBool(data){ return (data=="true" || data=="True" || data==true)?true:false;}
 
 	function setCfg(data){
 		//set config depending on option
 		data = "GT_"+data;
+		config[data]=!config[data];
+
 		switch(data){
 			case "GT_removeBanner":
-				config[data]=!config[data];
-
-				console.log("Setting banner to :"+config[data]);
-				if(!config[data]){
+				console.log("Setting "+data+" to :"+config[data]);
+				if(config[data]){
 					$("div#GT_removeBanner").removeClass("menuButtonGreen");$("div#GT_removeBanner").addClass("menuButtonRed");$("div#GT_removeBanner").text("Off");
 				}else{
 					$("div#GT_removeBanner").removeClass("menuButtonRed");$("div#GT_removeBanner").addClass("menuButtonGreen");$("div#GT_removeBanner").text("On")
 				}
 				break;
 
-			case "GT_gold":
-				config[data]=!config[data];
-
+			case "GT_getGold":
 				console.log("Setting gold to :"+config[data]);
-				if(!config[data]){
-					$("div#GT_gold").removeClass("menuButtonGreen");$("div#GT_gold").addClass("menuButtonRed");$("div#GT_gold").text("Off");
+				if(config[data]){
+					$("div#GT_getGold").removeClass("menuButtonGreen");$("div#GT_getGold").addClass("menuButtonRed");$("div#GT_getGold").text("Off");
 				}else{
-					$("div#GT_gold").removeClass("menuButtonRed");$("div#GT_gold").addClass("menuButtonGreen");$("div#GT_gold").text("On");
+					$("div#GT_getGold").removeClass("menuButtonRed");$("div#GT_getGold").addClass("menuButtonGreen");$("div#GT_getGold").text("On");
 				}
 				break;
 
 			case "GT_set24H":
-				config[data]=!config[data];
-
 				console.log("Setting listing to :"+config[data]);
-				if(!config[data]){
+				if(config[data]){
 					$("div#GT_set24H").removeClass("menuButtonGreen");$("div#GT_set24H").addClass("menuButtonRed");$("div#GT_set24H").text("Off");
 				}else{
 					$("div#GT_set24H").removeClass("menuButtonRed");$("div#GT_set24H").addClass("menuButtonGreen");$("div#GT_set24H").text("On");
@@ -134,11 +140,11 @@
 		saveCfg();
 
 		if(window.location.href.contains("mod=market")){
-			if(config.GT_set24H == true)
+			if(config.GT_set24H)
 				document.querySelector("#dauer").value = 3;
 		}
 		//removes banner
-		if(config.GT_removeBanner == true){
+		if(config.GT_removeBanner){
 			$("#banner_top").css("display","none");
 			$("#banner_event").css("display","none");
 			$("#banner_event_link").css("display","none");
@@ -146,7 +152,7 @@
 		}
 		if(window.location.href.contains("mod=packages")){
 			//collect all gold from packages
-			if(config.GT_getGold == true)
+			if(config.GT_getGold)
 				if($("div.item-i-14-1").length > 0){
 					var total = 0;
 					$("div.item-i-14-1").each(function(){
